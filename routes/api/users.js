4869597dc4d69b3b5e9c52d6ec9381b9e00d5eb3
hereiam123 <bdemaio1@gmail.com>
2018-10-03 20:41:48 -0400

    Added passport and started integrating authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+const passport = require("passport");
 
 //Load User model
 const User = require("../../models/User");
@@ -61,7 +64,21 @@ router.post("/login", (req, res) => {
       .compare(password, user.password)
       .then(isMatch => {
         if (isMatch) {
-          res.json({ msg: "Success" });
+          //User matched password
+          //Create JWT Payload
+          const payload = { id: user.id, name: user.name, avatar: user.avatar };
+          //Sign Token
+          jwt.sign(
+            payload,
+            keys.secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: "Bearer " + token
+              });
+            }
+          );
         } else {
           return res.status(400).json({ password: "Password Incorrect" });
         }
@@ -69,5 +86,16 @@ router.post("/login", (req, res) => {
       .catch(err => console.log(err));
   });
 });
+
+//@route GET api/users/current
+//@desc Return current user
+//@access Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({ msg: "Success" });
+  }
+);
 
 module.exports = router;
