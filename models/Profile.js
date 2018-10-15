62ccb5479d6f2967c5df0c2d0b36f1761cf594b6
hereiam123 <bdemaio1@gmail.com>
2018-10-15 18:31:14 -0400

    Building profile schema

--- models/Profile.js
+++ models/Profile.js
@@ -54,9 +54,67 @@ const ProfileSchema = new Schema({
       },
       to: {
         type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
       }
     }
-  ]
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
 });
 
-module.exports = User = mongoose.model("profile", ProfileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
