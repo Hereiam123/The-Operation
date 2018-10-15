274aa728212f7925080843075b304482ee32d0e0
hereiam123 <bdemaio1@gmail.com>
2018-10-15 18:49:05 -0400

    Creating default profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-//@route GET api/profile/test
-//@desc Tests profile route
-//@access Public
-router.get("/test", (req, res) => res.json({ msg: "Profile route works" }));
+//Load Profile Model
+const Profile = require("../../models/Profile");
+//Load user profile
+const User = require("../../models/User");
+
+//@route GET api/profile
+//@desc Get current users profile
+//@access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile located for this user";
+          res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
