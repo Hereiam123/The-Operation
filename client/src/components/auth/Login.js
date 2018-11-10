25c05aaeb099b06a1c7a2521df8483b24a088508
hereiam123 <bdemaio1@gmail.com>
2018-11-10 12:36:12 -0500

    Testing Register and Login page forms with axios

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,68 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const loginUser = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    axios
+      .post("/api/users/login", loginUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your MERNSocial account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    name="email"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    name="password"
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
