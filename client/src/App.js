0dc2ddb535776afd5e0ce059c0ad87d488f3a601
hereiam123 <bdemaio1@gmail.com>
2018-11-10 21:43:08 -0500

    Logout user if token is expired

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
 import store from "./store";
 import "./App.css";
@@ -20,6 +20,15 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken);
   //Set current user
   store.dispatch(setCurrentUser(decoded));
+  //Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp <= currentTime) {
+    //Logout User
+    store.dispatch(logoutUser());
+    //TODO: Clear current profile
+    //Redirect to Login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
