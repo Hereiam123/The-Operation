2b885d3caef0f7ed9fa94acd76aefd3c74fe3239
hereiam123 <bdemaio1@gmail.com>
2018-12-09 21:48:25 -0500

    Creating Profile(s) reducers and actions

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,3 +1,5 @@
+import { GET_PROFILE, PROFILE_LOADING } from "../actions/types";
+
 const initialState = {
   profile: null,
   profiles: null,
@@ -5,7 +7,18 @@ const initialState = {
 };
 
 export default function(state = initialState, action) {
-  switch (action) {
+  switch (action.type) {
+    case PROFILE_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    case GET_PROFILE:
+      return {
+        ...state,
+        profile: action.payload,
+        loading: false
+      };
     default:
       return state;
   }
