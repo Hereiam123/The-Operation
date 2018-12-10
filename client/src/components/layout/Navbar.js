308db52e4e8848f3e33e208ec8377566e5663012
hereiam123 <bdemaio1@gmail.com>
2018-12-09 22:07:38 -0500

    Pushing profile actions and reducers, clearing current profile on logout

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
+    this.props.clearCurrentProfile();
   };
 
   render() {
@@ -90,5 +92,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { logoutUser }
+  { logoutUser, clearCurrentProfile }
 )(Navbar);
