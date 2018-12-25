cdd83a8d0923e2ac6d574193d003e473c0743647
hereiam123 <bdemaio1@gmail.com>
2018-12-25 10:18:16 -0500

    Adding create profile component and route

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/common/PrivateRoute";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -50,6 +51,13 @@ class App extends Component {
               <Route exact path="/register" component={Register} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
 
