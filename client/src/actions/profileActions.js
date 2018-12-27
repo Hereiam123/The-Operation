6dea929ab3a14d1c0fbd84c260a17fe88cdfa82d
hereiam123 <bdemaio1@gmail.com>
2018-12-26 21:28:48 -0500

    Updated delete experience handling, now passing in experience id via ES6 syntax

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -56,6 +56,14 @@ export const addExperience = (expData, history) => dispatch => {
     .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
 };
 
+//Delete experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res => dispatch({ type: GET_PROFILE, payload: res.data }))
+    .catch(err => dispatch({ type: GET_ERRORS, payload: err.response.data }));
+};
+
 //Add education
 export const addEducation = (eduData, history) => dispatch => {
   axios
--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
 import Moment from "react-moment";
+import { deleteExperience } from "../../actions/profileActions";
+import PropTypes from "prop-types";
 
 class Experience extends Component {
+  onDeleteClick = id => {
+    this.props.deleteExperience(id);
+  };
+
   render() {
     const experience = this.props.experience.map(exp => (
       <tr key={exp._id}>
@@ -11,10 +16,19 @@ class Experience extends Component {
         <td>{exp.title}</td>
         <td>
           <Moment format="YYYY/MM/DD">{exp.from}</Moment> -{" "}
-          {exp.to === null ? "Current" : <Moment format="YYYY/MM/DD" />}
+          {exp.to === null ? (
+            "Current"
+          ) : (
+            <Moment format="YYYY/MM/DD">{exp.to}</Moment>
+          )}
         </td>
         <td>
-          <button className="btn btn-danger">Delete</button>
+          <button
+            onClick={() => this.onDeleteClick(exp._id)}
+            className="btn btn-danger"
+          >
+            Delete
+          </button>
         </td>
       </tr>
     ));
@@ -37,4 +51,11 @@ class Experience extends Component {
   }
 }
 
-export default connect(null)(withRouter(Experience));
+Experience.propTypes = {
+  deleteExperience: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { deleteExperience }
+)(Experience);
