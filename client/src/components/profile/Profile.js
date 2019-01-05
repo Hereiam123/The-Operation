40e9bab4f559833edcfb89e75a05619934f4c0ed
Brian De Maio <bdemaio1@gmail.com>
2019-01-05 17:18:17 -0500

    Minor bug fixes

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,66 +1,69 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
-import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
-import ProfileHeader from './ProfileHeader';
-import ProfileAbout from './ProfileAbout';
-import ProfileCreds from './ProfileCreds';
-import ProfileGitHub from './ProfileGitHub';
-import Spinner from '../common/Spinner';
-import {getProfileByHandle} from '../../actions/profileActions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import ProfileHeader from "./ProfileHeader";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileGitHub from "./ProfileGitHub";
+import Spinner from "../common/Spinner";
+import { getProfileByHandle } from "../../actions/profileActions";
 
 class Profile extends Component {
-componentDidMount(){
-    if(this.props.match.params.handle){
-        this.props.getProfileByHandle(this.props.match.params.handle)
-    } 
-}
+  componentDidMount() {
+    if (this.props.match.params.handle) {
+      this.props.getProfileByHandle(this.props.match.params.handle);
+    }
+  }
 
   render() {
-      const {profile, loading} = this.props.profile; 
-        let profileContent;
+    const { profile, loading } = this.props.profile;
+    let profileContent;
 
-        if(profile == null || loading){
-            profileContent = <Spinner/>
-        }
-        else{
-            profileContent = (
-                <div>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <Link to = "/profiles" className = "btn btn-light mb-3 float-left">
-                                Back to profiles
-                            </Link>
-                            <div className="col-md-6"></div>
-                        </div>
-                    </div>
-                    <ProfileHeader profile={profile}/>
-                    <ProfileAbout profile={profile}/>
-                    <ProfileCreds education = {profile.education} experience={profile.experience}/>
-                    <ProfileGitHub/>
-                </div>
-            )
-        }
-      return (
+    if (profile == null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back to profiles
+              </Link>
+              <div className="col-md-6" />
+            </div>
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          <ProfileGitHub />
+        </div>
+      );
+    }
+    return (
       <div className="profile">
         <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    {profileContent}
-                </div>
-            </div>
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 Profile.propTypes = {
-    profile: PropTypes.object.isRequired
-}
+  profile: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-    profile: state.profile
-})
+  profile: state.profile
+});
 
-export default connect(mapStateToProps, {getProfileByHandle})(Profile);
+export default connect(
+  mapStateToProps,
+  { getProfileByHandle }
+)(Profile);
