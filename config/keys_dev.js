a2d079c101e8f1256754c37be480bbd362dca168
Brian De Maio <bdemaio1@gmail.com>
2019-01-07 04:39:42 -0500

    Had to adjust keys

---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb://hereiam123:" +
+    encodeURIComponent("@youRay123") +
+    "@cluster0-shard-00-00-fkv3t.mongodb.net:27017,cluster0-shard-00-01-fkv3t.mongodb.net:27017,cluster0-shard-00-02-fkv3t.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
+  secretOrKey: "secret"
+};
