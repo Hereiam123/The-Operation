c614d0c686458ff5fa5fa886200c0d1bd44695bb
Brian De Maio <bdemaio1@gmail.com>
2019-01-07 05:29:01 -0500

    Config

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://hereiam123:l4XjZYkmS2z6fK8m@cluster0-shard-00-00-fkv3t.mongodb.net:27017,cluster0-shard-00-01-fkv3t.mongodb.net:27017,cluster0-shard-00-02-fkv3t.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",
-  secretOrKey: "secret"
-};
