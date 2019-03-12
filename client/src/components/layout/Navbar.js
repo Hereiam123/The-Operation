18a372a646c0a22c8c1daabf6951ca5d814ad421
hereiam123 <bdemaio1@gmail.com>
2019-03-12 01:41:36 -0400

    Heroku push

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -61,10 +61,10 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+      <nav className="navbar navbar-expand-sm navbar-light bg-light mb-4">
         <div className="container">
           <Link className="navbar-brand" to="/">
-            MERN Social
+            Dev Operations
           </Link>
           <button
             className="navbar-toggler"
