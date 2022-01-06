fa1f12dd56db9a88b9efb943e7b6cb3c2b98fad6
Brian De Maio <briandemaio@Brians-MacBook-Air.local>
2022-01-05 23:04:12 -0500

    Avatars and usernames link to corresponding profile, if one exists

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on ${port}`));
