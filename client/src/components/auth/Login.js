f783348e7e369d9cafa32621289c1d3ecfee2e7d
Brian De Maio <briandemaio@Brians-MacBook-Air.local>
2022-01-06 16:30:47 -0500

    Text display for Account Creation and Login now have uppercase letters

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -57,7 +57,7 @@ class Login extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Log In</h1>
               <p className="lead text-center">
-                Sign in to your Dev Operations account
+                Sign In To Your Dev Operations Account
               </p>
               <form onSubmit={this.onSubmit}>
                 <TextFieldGroup
